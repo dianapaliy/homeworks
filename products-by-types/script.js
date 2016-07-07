785f70440a9dee35a0db81c4ffe1ce74122b2572
user <paliy.diana@gmail.com>
2016-07-07 11:06:06 +0300

    Add script.js

--- products-by-types/script.js
+++ products-by-types/script.js
@@ -1,1 +1,38 @@
-// Ваш код
+'use strict';
+
+$.ajax({
+    'url': '/products-by-types/products.json',
+    dataType: 'json',
+    beforeSend: function () {
+        $.fancybox.showLoading();
+    }
+}).done(function (products) {
+    var $blockSale = $('#sale'),
+        $blockPromo = $('#promo'),
+        $blockRecommend = $('#recommended'),
+        $block = $('.block'),
+        $tmpl = $("#listTemplate");
+
+
+
+    var saleProducts = products.filter(function(item) {
+        return item.type == 'sale';
+    });
+
+    var promoProducts = products.filter(function(item) {
+        return item.type == 'promo';
+    });
+
+    var recommendProducts = products.filter(function(item) {
+        return item.type == 'recommended';
+    });
+
+    $blockSale.html(_.template($tmpl.html())({products: saleProducts}));
+    $blockPromo.html(_.template($tmpl.html())({products: promoProducts}));
+    $blockRecommend.html(_.template($tmpl.html())({products: recommendProducts}));
+
+}).fail(function (error) {
+    console.log(error);
+}).always(function () {
+    $.fancybox.hideLoading();
+});
