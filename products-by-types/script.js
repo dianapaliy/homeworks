376d256448f598eb193da734084ee9675b558a39
user <paliy.diana@gmail.com>
2016-07-07 11:47:53 +0300

    Fix ajax

--- products-by-types/script.js
+++ products-by-types/script.js
@@ -13,23 +13,25 @@ $.ajax({
         tmpl            = _.template($("#listTemplate").html());
 
     var saleProducts = products.filter(function(item) {
-        return item.type == 'sale';
+        return item.type === 'sale';
     });
 
     var promoProducts = products.filter(function(item) {
-        return item.type == 'promo';
+        return item.type === 'promo';
     });
 
     var recommendProducts = products.filter(function(item) {
-        return item.type == 'recommended';
+        return item.type === 'recommended';
     });
 
-    $blockSale.html(tmpl({products: saleProducts, type: 'Распродажа'}));
-    $blockPromo.html(tmpl({products: promoProducts, type: 'Промо-акция'}));
-    $blockRecommend.html(tmpl({products: recommendProducts, type: 'Рекомендуемые товары'}));
+    setTimeout(function() {
+        $blockSale.html(tmpl({products: saleProducts, type: 'Распродажа'}));
+        $blockPromo.html(tmpl({products: promoProducts, type: 'Промо-акция'}));
+        $blockRecommend.html(tmpl({products: recommendProducts, type: 'Рекомендуемые товары'}));
+        $.fancybox.hideLoading();
+    },1000);
 
 }).fail(function (error) {
     console.log(error);
-}).always(function () {
     $.fancybox.hideLoading();
 });
