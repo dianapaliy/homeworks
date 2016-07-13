9d373b05c73a7b9f1a4221d7e03119733ba9dfb5
user <paliy.diana@gmail.com>
2016-07-13 17:21:18 +0300

    Fix structure of project
    [re #master]

--- products-by-types/gulpfile.js
+++ products-by-types/gulpfile.js
@@ -5,23 +5,33 @@ var gulp         = require ('gulp'),
     concat       = require('gulp-concat'),
     watch        = require('gulp-watch'),
     plumber      = require('gulp-plumber'),
-    uglify = require('gulp-uglify'),
+    uglify       = require('gulp-uglify'),
+    notify       = require('gulp-notify'),
+    imagemin     = require('gulp-imagemin'),
+    minifyCSS   = require('gulp-minify-css'),
+    obfuscate   = require('gulp-obfuscate'),
+    sourcemaps = require('gulp-sourcemaps'),
+    eslint = require('gulp-eslint'),
+    reload       = browserSync.reload;
     env = process.env.NODE_ENV;
 
 var paths = {
-    scripts: './src/*.js',
-    styles: './src/*.less',
+    scripts: './src/js/*.js',
+    styles: './src/less/*.less',
     builtStyles: './build/css',
     builtJs: './build/js'
 };
 
-gulp.task('less', function() {
+gulp.task('styles', function() {
     return gulp.src(paths.styles)
         .pipe(less())
-        .pipe(uglify())
-        .pipe(plumber())
         .pipe(concat('all.css'))
-        .pipe(gulp.dest(paths.builtStyles));
+        .pipe(minifyCSS())
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.builtStyles))
+        .pipe(notify({ message: 'CSS task complete' }));
 });
 
 gulp.task('autoprefixer', function () {
@@ -32,29 +42,47 @@ gulp.task('autoprefixer', function () {
         .pipe(gulp.dest(paths.builtStyles));
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./build"
-        }
-    });
-});
-
 gulp.task('scripts', function() {
     return gulp.src(paths.scripts)
         .pipe(concat('all.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(paths.builtJs));
+        .pipe(obfuscate())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.builtJs))
+        .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-gulp.task('watch', function () {
-    return watch('build/css/*.css')
+gulp.task('copy', function () {
+    gulp.src(['./src/index.html', './src/products.json'])
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('copy-html', function () {
-    return src('src/*.html')
-        .pipe(gulp.dest('./build'));
+gulp.task('images', function() {
+    return gulp.src('./src/img/*')
+        .pipe(imagemin({ progressive: true }))
+        .pipe(gulp.dest('build/images-min'))
+        .pipe(notify({ message: 'Image task complete' }));
 });
 
-gulp.task('default', ['less'], ['autoprefixer'], ['browser-sync'], ['scripts'], ['watch'], ['copy-html']);
+gulp.task('watch', ['browser-sync'], function() {
+    gulp.watch('src/*.html', reload);
+    gulp.watch('src/less/*', ['styles']);
+    gulp.watch('src/js/*', ['scripts']);
+    gulp.watch('src/images/*', ['images']);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(['src/css/*', 'src/js/*'], {
+        server: {
+            baseDir: ["src"]
+        }
+    });
+});
+
+gulp.task('lint', function() {
+    return gulp.src(['src/js/*.js','src/less/*.less', '!node_modules/**'])
+        .pipe(eslint())
+});
+
+gulp.task('default', ['styles', 'autoprefixer', 'browser-sync', 'scripts', 'watch', 'copy', 'images', 'lint']);
